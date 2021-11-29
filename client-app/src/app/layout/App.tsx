a06c9495b986bf2c9cc7856ab320d5828728b42e
PROSUN619 <prosun.chakraborty92@gmail.com>
2021-11-29 08:25:13 +0530

    end of section 6 axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,12 @@
 import React, { Fragment, useEffect, useState } from 'react';
-//import { ducks } from './demo';
-//import { DuckItem } from './DuckItem';
-import axios from 'axios';
+//import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import { ActivityDashBoard } from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
 
@@ -14,11 +14,20 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {  // hook to set effect on state
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(Response => {
+    //axios.get<Activity[]>('http://localhost:5000/api/activities').then(Response => {
+    agent.Activities.list().then(Response => {
       //console.log(Response);
-      setActivities(Response.data);
+      let activities: Activity[] = [];
+      Response.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      });
+      setActivities(activities);
+      setLoading(false);
     })
   }, []) // use [] other wise it will be an endless loop
 
@@ -39,17 +48,43 @@ function App() {
     setEditMode(false);
   }
 
-  function handleCreateOrDeleteActivity(activity: Activity){
-    activity.id
-    ? setActivities([...activities.filter(x => x.id !== activity.id),activity])
-    :setActivities([...activities,{...activity, id: uuid()}]) // ... is spread operator set the guid
-    setEditMode(false);
-    setSelectedActivity(activity);
+  function handleCreateOrEditActivity(activity: Activity) {
+    setSubmitting(true);
+    if (activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setActivities([...activities.filter(x => x.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      });
+    }
+    else {
+        activity.id = uuid();
+        agent.Activities.create(activity).then(() => {
+          setActivities([...activities, activity]);
+          setSelectedActivity(activity);
+          setEditMode(false);
+          setSubmitting(false);    
+        });
+    }
+
+    // activity.id
+    //   ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+    //   : setActivities([...activities, { ...activity, id: uuid() }]) // ... is spread operator set the guid
+    // setEditMode(false);
+    // setSelectedActivity(activity);
   }
 
-  function handleDeleteActivity(id:string){
-    setActivities([...activities.filter(x => x.id !== id)]);
+  function handleDeleteActivity(id: string) {
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(x => x.id !== id)]);
+      setSubmitting(false);
+    });
+    
   }
+
+  if (loading) return <LoadingComponent content='Loading App' />
 
   return (
     <Fragment >
@@ -64,8 +99,9 @@ function App() {
           editMode={editMode}
           openForm={handleFormOpen}
           closeForm={handleFormClose}
-          createOrEdit={handleCreateOrDeleteActivity}
+          createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
     </Fragment>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -13,19 +13,21 @@ interface Props {
     editMode: boolean;
     openForm: (id: string) => void;
     closeForm: () => void;
-    createOrEdit:(activity : Activity) => void;
-    deleteActivity: (id:string) => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export function ActivityDashBoard({ activities, selectedActivity,
-    selectActivity, cancelSelectActivity,deleteActivity,
-    editMode, openForm, closeForm, createOrEdit }: Props) {
+    selectActivity, cancelSelectActivity, deleteActivity,
+    editMode, openForm, closeForm, createOrEdit, submitting }: Props) {
     return (
         <Grid>
             <GridColumn width="10">
-                <ActivityList activities={activities} 
-                selectActivity={selectActivity}
-                deleteActivity={deleteActivity}
+                <ActivityList activities={activities}
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </GridColumn>
             <GridColumn width="6">
@@ -35,8 +37,13 @@ export function ActivityDashBoard({ activities, selectedActivity,
                         cancelSelectActivity={cancelSelectActivity}
                         openForm={openForm}
                     />}
-                {editMode && 
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                {editMode &&
+                    <ActivityForm
+                        closeForm={closeForm}
+                        activity={selectedActivity}
+                        createOrEdit={createOrEdit} 
+                        submitting={submitting}
+                    />}
             </GridColumn>
         </Grid>
     )
